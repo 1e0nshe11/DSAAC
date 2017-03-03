4d11aa13ea7881c85b013be3d30267c0814788fb
NumberH <594165181@qq.com>
2017-03-03 13:45:49 +0800

    Update 1.3.h

--- ch1/1.3.h
+++ ch1/1.3.h
@@ -1,7 +1,7 @@
 #pragma once
 
 template<typename T>
-void fill(T*a int start, int end, const T&value)
+void fill(T*a，int start, int end, const T&value)
 {
 	for (int i = start; i != end; i++)
 		a[i] = value;
